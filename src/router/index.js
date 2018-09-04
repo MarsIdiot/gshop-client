373948173ef6336b5e7e88b280316eeb5f9d4dfa
tianlang.zhou <tianlang.zhou@ucarinc.com>
2018-09-04 16:56:22 +0800

    完成基本静态组件的构建(头部设计slot、轮播图swiper、 登录)

--- src/router/index.js
+++ src/router/index.js
@@ -12,29 +12,48 @@ import MiSite from '../pages/MiSite/MiSite.vue'
 import Search from '../pages/Search/Search.vue'
 import  Order from  '../pages/Order/Order.vue'
 import  Profile from  '../pages/Profile/Profile.vue'
+import Login from '../pages/Login/Login.vue'
 
 export default new VueRouter({
   routes:[
     {
       path:'/miSite',
-      component:MiSite
+      component:MiSite,
+      meta:{
+        showFooter:true
+      }
     },
     {
       path:'/search',
-      component:Search
+      component:Search,
+      meta:{
+        showFooter:true
+      }
     },
     {
       path:'/order',
-      component:Order
+      component:Order,
+      meta:{
+        showFooter:true
+      }
     },
     {
       path:'/profile',
-      component:Profile
+      component:Profile,
+      meta:{
+        showFooter:true
+      }
     },
+
+    {
+      path:'/login',
+      component:Login
+    },
+
     //配置自动定位首页
     {
       path:'/',
-      redirect:'/miSite'
+      redirect:'/miSite',
     }
 
   ]
